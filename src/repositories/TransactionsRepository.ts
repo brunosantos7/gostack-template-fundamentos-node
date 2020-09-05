449b0a8292609f3a919a52bbf899fd2f731e73bb
Bruno da Silveira Santos <bsantos@synvata.com>
2020-09-04 23:12:14 -0300

    Creating the apis to save and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 import Transaction from '../models/Transaction';
 
 interface Balance {
@@ -5,6 +6,12 @@ interface Balance {
   outcome: number;
   total: number;
 }
+
+type CreateTransactionData = {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+};
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +21,41 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total } as Balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(transaction: CreateTransactionData): Transaction {
+    const newTransaction = new Transaction({ ...transaction });
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+type CreateTransactionData = {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+};
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,13 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(transaction: CreateTransactionData): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (transaction.type === 'outcome' && transaction.value > total)
+      throw new Error();
+
+    return this.transactionsRepository.create(transaction);
   }
 }
 
